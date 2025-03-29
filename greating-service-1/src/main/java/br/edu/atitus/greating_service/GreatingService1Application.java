f01692aad335e6d7e64f09073450ecd7fbf336cd
Gabe <gquadrosa2@gmail.com>
2025-03-28 22:02:41 -0300

    Greating Service

---
+++ greating-service-1/src/main/java/br/edu/atitus/greating_service/GreatingService1Application.java
@@ -0,0 +1,13 @@
+package br.edu.atitus.greating_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreatingService1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreatingService1Application.class, args);
+	}
+
+}
